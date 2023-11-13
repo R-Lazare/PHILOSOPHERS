2e9821fb323a6cb1bf29b7146b612c60215fb4c5
Robin Luiz <rluiz@c2r3p2.42lehavre.fr>
2023-11-13 19:15:46 +0100

    f

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: rluiz <rluiz@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 18:59:58 by rluiz             #+#    #+#             */
-/*   Updated: 2023/11/13 19:02:43 by rluiz            ###   ########.fr       */
+/*   Updated: 2023/11/13 19:13:31 by rluiz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,61 @@ typedef struct s_fork
 	int					fork_id;
 	int					is_taken;
 }						t_fork;
+
+typedef struct s_philo
+{
+	int					id;
+	int					meals;
+	int					is_eating;
+	int					is_dead;
+	int					is_full;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					num_of_meals;
+	int					num_of_philos;
+	int					time_start;
+	int					time_last_meal;
+	t_fork				*left_fork;
+	t_fork				*right_fork;
+	t_table				*table;
+	pthread_mutex_t		*death_mutex;
+	pthread_mutex_t		*eat_mutex;
+	pthread_mutex_t		*sleep_mutex;
+	pthread_mutex_t		*think_mutex;
+	pthread_mutex_t		*fork_mutex;
+	pthread_t			philo_thread_id;
+}						t_philo;
+
+typedef struct s_table
+{
+	int					num_of_philos;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					num_of_meals;
+	int					time_start;
+	int					philos_full;
+	t_philo				*philos;
+	t_fork				*forks;
+	t_arena				*arena;
+	pthread_mutex_t		*death_mutex;
+	pthread_mutex_t		*table_mutex;
+	pthread_mutex_t		*print_mutex;
+}						t_table;
+
+void					*arena_init(size_t buffer_size);
+void					arena_reset(t_arena *a);
+void					*arena_alloc(t_arena *a, size_t size);
+void					arena_destroy(t_arena *a);
+void					error_exit(char *error, pthread_mutex_t *print,
+							t_arena *arena);
+int						ft_printf(t_table *table, const char *src, ...);
+int						ft_atoi(const char *str);
+char					*ft_itoa(t_arena *arena, int n);
+int						get_time_ms(t_table *table);
+void					ft_putstr_fd(char *s, int fd);
+void					ft_putchar_fd(char c, int fd);
+void					*philo_life(void *philo);
 
 #endif
